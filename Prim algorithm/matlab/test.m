9f4a8c12917865549748fa6e67dde48f65d7dcda
yifan <568413586@qq.com>
2017-08-17 23:21:22 +0800

    Prim

---
+++ Prim algorithm/matlab/test.m
@@ -0,0 +1,8 @@
+W=[0 7 inf 5 inf inf inf 
+    7 0 8 9 7 inf inf 
+    inf 8 0 inf 5 inf inf  
+    5 9 inf 0 15 6 inf  
+    inf 7 5 15 0 8 9  
+    inf inf inf 6 8 0 11
+    inf inf inf inf 9 11 0];
+MST = MinST(W,4)
